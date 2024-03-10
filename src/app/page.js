3946a3df8d0678f0205c5d71387b651ea5595be0
Anthony Cowan <anthony.cowan@expandtheroom.com>
2024-03-09 19:42:59 -0600

    Testing

--- src/app/page.js
+++ src/app/page.js
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            David is a bitch
           </p>
         </a>
 
@@ -104,7 +104,8 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            daivd Instantly deploy your Next.js site to a shareable URL with Vercel.
+            daivd Instantly deploy your Next.js site to a shareable URL with
+            Vercel.
           </p>
         </a>
       </div>
